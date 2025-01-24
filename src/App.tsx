b16b0e0d500a5e07df1035ea8a721ba44cf8d5ba
Ridho <reedho@gmail.com>
2025-01-25 06:37:21 +0700

    Verify all is well

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { useId } from "react"
+import { XDateRangePicker } from "./uix/date-range-picker"
+
 export default function App() {
+  const id = useId()
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello</h1>
-      <p>You made it...</p>
+      <p>Lets make!</p>
+
+      <br />
+
+      <div className="max-w-sm">
+        <div className="space-y-2">
+          <Label htmlFor={id}>Email</Label>
+          <Input id={id} placeholder="Email" type="email" />
+        </div>
+
+        <br />
+
+        <XDateRangePicker />
+      </div>
     </div>
   )
 }
